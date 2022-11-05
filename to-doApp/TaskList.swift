aa7e8c21a04e139d8250a648c8add9f8d05f0486
Pedro Grando <grandop03@gmail.com>
2022-11-05 17:43:30 -0300

    Fazendo o layout

---
+++ to-doApp/TaskList.swift
@@ -0,0 +1,17 @@
+//
+//  taskList.swift
+//  to-doApp
+//
+//  Created by Pedro Grando on 05/11/22.
+//
+
+import UIKit
+
+struct TaskList {
+    var taskNameStructed: String
+    
+    
+    init(taskNameStructed: String) {
+        self.taskNameStructed = taskNameStructed
+    }
+}
--- to-doApp/ViewController.swift
+++ to-doApp/ViewController.swift
@@ -7,13 +7,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var allTasks: [TaskList] = []
+    let spacingBetweenSecions: CGFloat = 100.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        allTasks = addingTasks()
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
+    
+    
+    func addingTasks() -> [TaskList] {
+        return [
+            TaskList(taskNameStructed: "Limpar a casa"),
+            TaskList(taskNameStructed: "Limpar a casa"),
+            TaskList(taskNameStructed: "Limpar a casa"),
+            TaskList(taskNameStructed: "Limpar a casa"),
+            TaskList(taskNameStructed: "Limpar a casa"),
+            TaskList(taskNameStructed: "Limpar a casa"),
+            TaskList(taskNameStructed: "Limpar a casa"),
+            TaskList(taskNameStructed: "Limpar a casa"),
+            TaskList(taskNameStructed: "Limpar a casa"),
+            TaskList(taskNameStructed: "Limpar a casa"),
+            TaskList(taskNameStructed: "Limpar a casa"),
+            TaskList(taskNameStructed: "Limpar a casa"),
+            TaskList(taskNameStructed: "Limpar a casa"),
+            TaskList(taskNameStructed: "Limpar a casa"),
+            TaskList(taskNameStructed: "Limpar a casa"),
+            TaskList(taskNameStructed: "Limpar a casa"),
+            TaskList(taskNameStructed: "Limpar a casa"),
+            TaskList(taskNameStructed: "Limpar a casa"),
+            TaskList(taskNameStructed: "Limpar a casa"),
+            TaskList(taskNameStructed: "Limpar a casa"),
+            TaskList(taskNameStructed: "Limpar a casa"),
+            TaskList(taskNameStructed: "Limpar a casa")
+            
+        ]
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allTasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: TaskCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TaskCell
+        
+        
+        cell.backgroundColor = UIColor.white
+        cell.layer.borderColor = UIColor.black.cgColor
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 15
+        cell.clipsToBounds = true
 
+        
+        cell.addingTaskOnList(task: allTasks[indexPath.row])
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("apertado \(allTasks[indexPath.row])")
+    }
+    
 
 }
 
