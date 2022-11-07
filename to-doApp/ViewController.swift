4ab37ababed05bf33fad292f69a7f60a9e32847e
Pedro Grando <grandop03@gmail.com>
2022-11-07 00:09:24 -0300

    Terminando o exercício

--- to-doApp/ViewController.swift
+++ to-doApp/ViewController.swift
@@ -8,74 +8,71 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
+
+    @IBOutlet weak var taskTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
-    var allTasks: [TaskList] = []
-    let spacingBetweenSecions: CGFloat = 100.0
+
+    var tasksList: [String] = []
+    let tasksListKey = "tasksListKey"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        allTasks = addingTasks()
+        tasksList = UserDefaults.standard.stringArray(forKey: tasksListKey) ?? []
+        
         tableView.dataSource = self
         tableView.delegate = self
-        
     }
-    
-    
-    func addingTasks() -> [TaskList] {
-        return [
-            TaskList(taskNameStructed: "Limpar a casa"),
-            TaskList(taskNameStructed: "Limpar a casa"),
-            TaskList(taskNameStructed: "Limpar a casa"),
-            TaskList(taskNameStructed: "Limpar a casa"),
-            TaskList(taskNameStructed: "Limpar a casa"),
-            TaskList(taskNameStructed: "Limpar a casa"),
-            TaskList(taskNameStructed: "Limpar a casa"),
-            TaskList(taskNameStructed: "Limpar a casa"),
-            TaskList(taskNameStructed: "Limpar a casa"),
-            TaskList(taskNameStructed: "Limpar a casa"),
-            TaskList(taskNameStructed: "Limpar a casa"),
-            TaskList(taskNameStructed: "Limpar a casa"),
-            TaskList(taskNameStructed: "Limpar a casa"),
-            TaskList(taskNameStructed: "Limpar a casa"),
-            TaskList(taskNameStructed: "Limpar a casa"),
-            TaskList(taskNameStructed: "Limpar a casa"),
-            TaskList(taskNameStructed: "Limpar a casa"),
-            TaskList(taskNameStructed: "Limpar a casa"),
-            TaskList(taskNameStructed: "Limpar a casa"),
-            TaskList(taskNameStructed: "Limpar a casa"),
-            TaskList(taskNameStructed: "Limpar a casa"),
-            TaskList(taskNameStructed: "Limpar a casa")
-            
-        ]
-    }
-    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return allTasks.count
+        return tasksList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: TaskCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TaskCell
         
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+       
         cell.backgroundColor = UIColor.white
         cell.layer.borderColor = UIColor.black.cgColor
         cell.layer.borderWidth = 1
         cell.layer.cornerRadius = 15
-        cell.clipsToBounds = true
+        cell.clipsToBounds = false
+        
+        cell.textLabel?.text = tasksList[indexPath.row]
 
-        
-        cell.addingTaskOnList(task: allTasks[indexPath.row])
-        
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("apertado \(allTasks[indexPath.row])")
+    @IBAction func buttonToAddTask(_ sender: UIButton) {
+        let task = String(taskTextField.text!)
+        
+        tasksList.append(task)
+        setStorageTasks()
+        tableView.reloadData()
+        
+        taskTextField.text = ""
     }
     
+    @IBAction func buttonToRemoveTask(_ sender: UIButton) {
+        var superview = sender.superview
+            while let view = superview, !(view is UITableViewCell) {
+                superview = view.superview
+            }
+            guard let cell = superview as? UITableViewCell else {
+                return
+            }
+            guard let indexPath = tableView.indexPath(for: cell) else {
+                return
+            }
 
+        tasksList.remove(at: indexPath.row)
+        tableView.reloadData()
+        setStorageTasks()
+    }
+
+    func setStorageTasks() {
+        UserDefaults.standard.set(tasksList, forKey: tasksListKey)
+    }
+    
 }
 
